c863345c9ae00bc94aa710caf3355e7e51b5055c
이 종원 <jong12em@naver.com>
2020-05-27 15:42:41 +0900

    [버그] 다중 필터 적용시 생기던 잡다한 오류 해결
    1. 두가지 이상 필터 적용시 예상과 다른 이미지 도출 해결
    
    2. addFilter 함수 단순화
    
    3. progressBar 사용 시 이미지에 필터 적용되도록 수정

--- app/src/main/java/capstone/aiimageeditor/ui/FragmentPerson.kt
+++ app/src/main/java/capstone/aiimageeditor/ui/FragmentPerson.kt
@@ -30,13 +30,17 @@ class FragmentPerson : Fragment() {
     private lateinit var imageManager: ImageManager
     private lateinit var maskSeparator: MaskSeparator
     private var filters = arrayListOf<GPUImageFilter?>()
-
+    private var adjusts = arrayListOf<Int>()
+    private var tabPosition=0
     private var filterAdjuster: GPUImageFilterTools.FilterAdjuster? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        for(i in 0 .. 8) filters.add(null)
+        for(i in 0 .. 9) {
+            filters.add(null)
+            adjusts.add(50)
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -60,6 +64,8 @@ class FragmentPerson : Fragment() {
 
         seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                adjusts[tabPosition]=progress
+                filterAdjuster = GPUImageFilterTools.FilterAdjuster(filters[tabPosition]!!)
                 filterAdjuster?.adjust(progress)
                 //gpuImage.requestRender()
                 imageFG.setImageBitmap(gpuImage.getBitmapWithFiltersApplied(imageManager.person,filters))
@@ -87,12 +93,19 @@ class FragmentPerson : Fragment() {
     }
 
 
-    private fun addFilter(filter: GPUImageFilter,index:Int) {
-        filters[index]= filter
+    private fun addFilter(f: GPUImageFilter,index:Int) {
+        var filter = f
+        if(filters[index]!=null){
+            filter = filters[index]!!
+            seekBar.progress = adjusts[index];
+        }else
+        {
+            filters[index]=f
+        }
+
         filterAdjuster = GPUImageFilterTools.FilterAdjuster(filter)
         if (filterAdjuster!!.canAdjust()) {
             seekBar.visibility = View.VISIBLE
-            filterAdjuster!!.adjust(seekBar.progress)
         } else {
             seekBar.visibility = View.GONE
         }
@@ -105,12 +118,13 @@ class FragmentPerson : Fragment() {
         override fun onTabUnselected(tab: TabLayout.Tab?) {}
 
         override fun onTabSelected(tab: TabLayout.Tab?) {
-
+            imageFG.setImageBitmap(gpuImage.getBitmapWithFiltersApplied(imageManager.person,filters))
             seekBar.visibility=View.VISIBLE
-            seekBar.progress=50
+            tabPosition= tab!!.position
             when(tab?.position){
                 0->{
-                    gpuImage.setFilter(null)
+                    //gpuImage.setFilter(null)
+                    imageFG.setImageBitmap(imageManager.person)
                     seekBar.visibility=View.GONE
                 }
                 1-> addFilter(GPUImageFilterTools.createFilterForType(context!!,GPUImageFilterTools.FilterType.BRIGHTNESS),1)
@@ -122,8 +136,8 @@ class FragmentPerson : Fragment() {
                 7-> addFilter(GPUImageFilterTools.createFilterForType(context!!,GPUImageFilterTools.FilterType.SHARPEN),7)
                 8-> addFilter(GPUImageFilterTools.createFilterForType(context!!,GPUImageFilterTools.FilterType.GAMMA),8)
             }
+            seekBar.progress=adjusts[tabPosition]
             //gpuImage.requestRender()
-            imageFG.setImageBitmap(gpuImage.getBitmapWithFiltersApplied(imageManager.person,filters))
         }
 
     }
