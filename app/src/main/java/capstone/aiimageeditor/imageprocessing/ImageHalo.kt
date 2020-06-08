8ee1a090c643e3b8e117026c1a8a897a92626331
samsohn0128 <samsohn0128@gmail.com>
2020-06-09 00:52:30 +0900

    [기능] 후광 효과 추가
    인물 탭 안에 탭의 작동 방식을 정확히 몰라 아직 적용은 못함

--- app/src/main/java/capstone/aiimageeditor/imageprocessing/ImageHalo.kt
+++ app/src/main/java/capstone/aiimageeditor/imageprocessing/ImageHalo.kt
@@ -2,61 +2,313 @@ package capstone.aiimageeditor
 
 import android.graphics.Bitmap
 import android.graphics.Color
+import android.os.Build
+import android.util.Log
+import androidx.annotation.RequiresApi
 import java.util.*
 
-class ImageHalo(inputMask: Bitmap, inputImage: Bitmap) {
+class ImageHalo() {
+    private lateinit var colors: IntArray
 
-    private var mask = inputMask
-    private var image = inputImage
-    private var coordQueue: Queue<Pair<Int, Int>> = LinkedList<Pair<Int, Int>>()
+    private var coordQueue: Queue<Triple<Int, Int, Pair<Int, Int>>> = LinkedList()
+    private var width: Int = 0
+    private var height: Int = 0
+    private var bool = false
 
-    fun setHalo() {
-        val colors = IntArray(mask.width * mask.height)
-        mask.getPixels(colors, 0, mask.width, 0, 0, mask.width, mask.height)
+    fun setHalo(inputImage: Bitmap, inputMask: Bitmap): Bitmap {
+        colors = IntArray(inputMask.width * inputMask.height)
+        inputMask.getPixels(colors, 0, inputMask.width, 0, 0, inputMask.width, inputMask.height)
+        width = inputMask.width
+        height = inputMask.height
+        bool = false
+
         for (i in colors.indices) {
-            val y = i / mask.width
-            val x = i % mask.width
-            if (Color.red(colors[i]) != 0) {
-                if (i > 0 && Color.red(colors[i - 1]) != 0)
-                    coordQueue.add(Pair<Int, Int>(y, x))
-                else if (Color.red(colors[i + 1]) != 0)
-                    coordQueue.add(Pair<Int, Int>(y, x))
-                else if (i >= mask.width && Color.red(colors[i - mask.width]) != 0)
-                    coordQueue.add(Pair<Int, Int>(y, x))
-                else if (Color.red(colors[i + mask.width]) != 0)
-                    coordQueue.add(Pair<Int, Int>(y, x))
-                else if (i >= mask.width - 1 && Color.red(colors[i - mask.width + 1]) != 0)
-                    coordQueue.add(Pair<Int, Int>(y, x))
-                else if (Color.red(colors[i + mask.width + 1]) != 0)
-                    coordQueue.add(Pair<Int, Int>(y, x))
-                else if (i >= mask.width - 1 && Color.red(colors[i - mask.width - 1]) != 0)
-                    coordQueue.add(Pair<Int, Int>(y, x))
-                else if (Color.red(colors[i + mask.width - 1]) != 0)
-                    coordQueue.add(Pair<Int, Int>(y, x))
+            if (colors[i] == Color.RED) {
+                val x = i % width
+                val y = i / width
+                val xDirection = checkX(x, i)
+                val yDirection = checkY(y, i)
+                if (xDirection != -1 || yDirection != -1)
+                    coordQueue.add(Triple(x, y, Pair(xDirection, yDirection)))
             }
         }
+
         while (coordQueue.isNotEmpty()) {
             val temp = coordQueue.remove()
-            val y = temp.first
-            val x = temp.second
-            val i = y * mask.width + x
+            val x = temp.first
+            val y = temp.second
+            val i = y * inputMask.width + x
+            val xDirection = temp.third.first
+            val yDirection = temp.third.second
+            val widthMax = inputMask.width / 75
+            val heightMax = inputMask.height / 75
+            val alphaMax = (widthMax + heightMax)
 
-            if (i > 0 && Color.red(colors[i - 1]) == 0)
-                image.setPixel(x - 1, y, Color.WHITE)
-            if (Color.red(colors[i + 1]) == 0)
-                image.setPixel(x + 1, y, Color.WHITE)
-            if (i >= mask.width && Color.red(colors[i - mask.width]) == 0)
-                image.setPixel(x, y - 1, Color.WHITE)
-            if (Color.red(colors[i + mask.width]) == 0)
-                image.setPixel(x, y + 1, Color.WHITE)
-            if (i >= mask.width - 1 && Color.red(colors[i - mask.width + 1]) == 0)
-                image.setPixel(x + 1, y - 1, Color.WHITE)
-            if (Color.red(colors[i + mask.width + 1]) == 0)
-                image.setPixel(x + 1, y + 1, Color.WHITE)
-            if (i >= mask.width - 1 && Color.red(colors[i - mask.width - 1]) == 0)
-                image.setPixel(x - 1, y - 1, Color.WHITE)
-            if (Color.red(colors[i + mask.width - 1]) == 0)
-                image.setPixel(x - 1, y + 1, Color.WHITE)
+            if (yDirection == -1) {
+                when (xDirection) {
+                    0 -> {
+                        for (w in 0 until widthMax) {
+                            if (x > w) {
+                                val newColor = Color.argb(255 * (widthMax - w) / widthMax, 0, 255, 255)
+                                inputImage.setPixel(x - w, y, newColor)
+                            }
+                        }
+                    }
+                    1 -> {
+                        for (w in 0 until widthMax) {
+                            if (x < width - w - 1) {
+                                val newColor = Color.argb(255 * (widthMax - w) / widthMax, 0, 255, 255)
+                                inputImage.setPixel(x + w, y, newColor)
+                            }
+                        }
+                    }
+                    2 -> {
+                        for (w in 0 until widthMax) {
+                            if (x > w) {
+                                val newColor = Color.argb(255 * (widthMax - w) / widthMax, 0, 255, 255)
+                                inputImage.setPixel(x - w, y, newColor)
+                            }
+                            if (x < width - w - 1) {
+                                val newColor = Color.argb(255 * (widthMax - w) / widthMax, 0, 255, 255)
+                                inputImage.setPixel(x + w, y, newColor)
+                            }
+                        }
+                    }
+                }
+            } else if (xDirection == -1) {
+                when (yDirection) {
+                    0 -> {
+                        for (h in 0 until heightMax) {
+                            if (y > h) {
+                                val newColor = Color.argb(255 * (heightMax - h) / heightMax, 0, 255, 255)
+                                inputImage.setPixel(x, y - h, newColor)
+                            }
+                        }
+                    }
+                    1 -> {
+                        for (h in 0 until heightMax) {
+                            if (y < height - h - 1) {
+                                val newColor = Color.argb(255 * (heightMax - h) / heightMax, 0, 255, 255)
+                                inputImage.setPixel(x, y + h, newColor)
+                            }
+                        }
+                    }
+                    2 -> {
+                        for (h in 0 until heightMax) {
+                            if (y > h) {
+                                val newColor = Color.argb(255 * (heightMax - h) / heightMax, 0, 255, 255)
+                                inputImage.setPixel(x, y - h, newColor)
+                            }
+                            if (y < height - h - 1) {
+                                val newColor = Color.argb(255 * (heightMax - h) / heightMax, 0, 255, 255)
+                                inputImage.setPixel(x, y + h, newColor)
+                            }
+                        }
+                    }
+                }
+            } else {
+                when (xDirection) {
+                    0 -> {
+                        for (w in 0 until widthMax) {
+                            if (x > w) {
+                                when (yDirection) {
+                                    0 -> {
+                                        for (h in 0 until heightMax) {
+                                            if (y > h) {
+                                                val newColor = Color.argb(255 * (alphaMax - h - w) / alphaMax, 0, 255, 255)
+                                                inputImage.setPixel(x - w, y - h, newColor)
+                                            }
+                                        }
+                                    }
+                                    1 -> {
+                                        for (h in 0 until heightMax) {
+                                            if (y < height - h - 1) {
+                                                val newColor = Color.argb(255 * (alphaMax - h - w) / alphaMax, 0, 255, 255)
+                                                inputImage.setPixel(x - w, y + h, newColor)
+                                            }
+                                        }
+                                    }
+                                    2 -> {
+                                        for (h in 0 until heightMax) {
+                                            if (y > h) {
+                                                val newColor = Color.argb(255 * (alphaMax - h - w) / alphaMax, 0, 255, 255)
+                                                inputImage.setPixel(x - w, y - h, newColor)
+                                            }
+                                            if (y < height - h - 1) {
+                                                val newColor = Color.argb(255 * (alphaMax - h - w) / alphaMax, 0, 255, 255)
+                                                inputImage.setPixel(x - w, y + h, newColor)
+                                            }
+                                        }
+                                        Log.i("[xDyD]", "이런게 있네02")
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    1 -> {
+                        for (w in 0 until widthMax) {
+                            if (x < width - w - 1) {
+                                when (yDirection) {
+                                    0 -> {
+                                        for (h in 0 until heightMax) {
+                                            if (y > h) {
+                                                val newColor = Color.argb(255 * (alphaMax - h - w) / alphaMax, 0, 255, 255)
+                                                inputImage.setPixel(x + w, y - h, newColor)
+                                            }
+                                        }
+                                    }
+                                    1 -> {
+                                        for (h in 0 until heightMax) {
+                                            if (y < height - h - 1) {
+                                                val newColor = Color.argb(255 * (alphaMax - h - w) / alphaMax, 0, 255, 255)
+                                                inputImage.setPixel(x + w, y + h, newColor)
+                                            }
+                                        }
+                                    }
+                                    2 -> {
+                                        for (h in 0 until heightMax) {
+                                            if (y > h) {
+                                                val newColor = Color.argb(255 * (alphaMax - h - w) / alphaMax, 0, 255, 255)
+                                                inputImage.setPixel(x + w, y - h, newColor)
+                                            }
+                                            if (y < height - h - 1) {
+                                                val newColor = Color.argb(255 * (alphaMax - h - w) / alphaMax, 0, 255, 255)
+                                                inputImage.setPixel(x + w, y + h, newColor)
+                                            }
+                                        }
+                                        Log.i("[xDyD]", "이런게 있네12")
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    2 -> {
+                        for (w in 0 until widthMax) {
+                            if (x > w) {
+                                when (yDirection) {
+                                    0 -> {
+                                        for (h in 0 until heightMax) {
+                                            if (y > h) {
+                                                val newColor = Color.argb(255 * (alphaMax - h - w) / alphaMax, 0, 255, 255)
+                                                inputImage.setPixel(x - w, y - h, newColor)
+                                            }
+                                        }
+                                    }
+                                    1 -> {
+                                        for (h in 0 until heightMax) {
+                                            if (y < height - h - 1) {
+                                                val newColor = Color.argb(255 * (alphaMax - h - w) / alphaMax, 0, 255, 255)
+                                                inputImage.setPixel(x - w, y + h, newColor)
+                                            }
+                                        }
+                                    }
+                                    2 -> {
+                                        for (h in 0 until heightMax) {
+                                            if (y > h) {
+                                                val newColor = Color.argb(255 * (alphaMax - h - w) / alphaMax, 0, 255, 255)
+                                                inputImage.setPixel(x - w, y - h, newColor)
+                                            }
+                                            if (y < height - h - 1) {
+                                                val newColor = Color.argb(255 * (alphaMax - h - w) / alphaMax, 0, 255, 255)
+                                                inputImage.setPixel(x - w, y + h, newColor)
+                                            }
+                                        }
+                                        Log.i("[xDyD]", "이런게 있네22")
+                                    }
+                                }
+                            }
+                            if (x < width - w - 1) {
+                                when (yDirection) {
+                                    0 -> {
+                                        for (h in 0 until heightMax) {
+                                            if (y > h) {
+                                                val newColor = Color.argb(255 * (alphaMax - h - w) / alphaMax, 0, 255, 255)
+                                                inputImage.setPixel(x + w, y - h, newColor)
+                                            }
+                                        }
+                                    }
+                                    1 -> {
+                                        for (h in 0 until heightMax) {
+                                            if (y < height - h - 1) {
+                                                val newColor = Color.argb(255 * (alphaMax - h - w) / alphaMax, 0, 255, 255)
+                                                inputImage.setPixel(x + w, y + h, newColor)
+                                            }
+                                        }
+                                    }
+                                    2 -> {
+                                        for (h in 0 until heightMax) {
+                                            if (y > h) {
+                                                val newColor = Color.argb(255 * (alphaMax - h - w) / alphaMax, 0, 255, 255)
+                                                inputImage.setPixel(x + w, y - h, newColor)
+                                            }
+                                            if (y < height - h - 1) {
+                                                val newColor = Color.argb(255 * (alphaMax - h - w) / alphaMax, 0, 255, 255)
+                                                inputImage.setPixel(x + w, y + h, newColor)
+                                            }
+                                        }
+                                        Log.i("[xDyD]", "이런게 있네12")
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return inputImage
+    }
+
+    private fun checkX(x: Int, i: Int): Int {
+        when (x) {
+            0 -> {
+                return if (colors[i + 1] != Color.RED)
+                    1
+                else
+                    -1
+            }
+            height - 1 -> {
+                return if (colors[i - 1] != Color.RED)
+                    0
+                else
+                    -1
+            }
+            else -> {
+                return if (colors[i - 1] != Color.RED && colors[i + 1] != Color.RED)
+                    2
+                else if (colors[i - 1] != Color.RED)
+                    0
+                else if (colors[i + 1] != Color.RED)
+                    1
+                else
+                    -1
+            }
+        }
+    }
+
+    private fun checkY(y: Int, i: Int): Int {
+        when (y) {
+            0 -> {
+                return if (colors[i + width] != Color.RED)
+                    1
+                else
+                    -1
+            }
+            height - 1 -> {
+                return if (colors[i - width] != Color.RED)
+                    0
+                else
+                    -1
+            }
+            else -> {
+                return if (colors[i - width] != Color.RED && colors[i + width] != Color.RED)
+                    2
+                else if (colors[i - width] != Color.RED)
+                    0
+                else if (colors[i + width] != Color.RED)
+                    1
+                else
+                    -1
+            }
         }
     }
 }
--- app/src/main/java/capstone/aiimageeditor/ui/FragmentPerson.kt
+++ app/src/main/java/capstone/aiimageeditor/ui/FragmentPerson.kt
@@ -1,13 +1,20 @@
 package capstone.aiimageeditor.ui
 
+import android.content.Intent
+import android.graphics.Bitmap
 import android.graphics.Matrix
 import android.graphics.RectF
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.*
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.SeekBar
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
+import capstone.aiimageeditor.ImageHalo
 import capstone.aiimageeditor.ImageManager
 import capstone.aiimageeditor.R
 import capstone.aiimageeditor.imageprocessing.GPUImageFilterTools
@@ -29,20 +36,21 @@ class FragmentPerson : Fragment() {
     private lateinit var gpuImage: GPUImage
     private lateinit var tabLayout: TabLayout
     private lateinit var imageManager: ImageManager
+    private lateinit var imageHalo: ImageHalo
 
     private var filters = arrayListOf<GPUImageFilter?>()
     private var adjusts = arrayListOf<Int>()
-    private var tabPosition=0
+    private var tabPosition = 0
     private var filterAdjuster: GPUImageFilterTools.FilterAdjuster? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        for(i in 0 .. 8) {
+        for (i in 0..8) {
             filters.add(null)
             adjusts.add(50)
         }
-        adjusts[4]=0
+        adjusts[4] = 0
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -50,28 +58,31 @@ class FragmentPerson : Fragment() {
         seekBar = view.findViewById(R.id.seekBar)
         imageBG = view.findViewById(R.id.image_bg)
         imageFG = view.findViewById(R.id.image_fg)
+//        btnHalo = view.findViewById(R.id.btnHalo)
         tabLayout = view.findViewById(R.id.tabLayout)
 
         imageManager = (activity?.application as ImageManager)
+        imageHalo = ImageHalo()
 
         gpuImage = GPUImage(context)
 
-
-
-
-
-        imageBG.visibility=View.VISIBLE
-        seekBar.max=100
-        seekBar.progress=50
-        seekBar.visibility=View.GONE
+        imageBG.visibility = View.VISIBLE
+        seekBar.max = 100
+        seekBar.progress = 50
+        seekBar.visibility = View.GONE
 
         seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                adjusts[tabPosition]=progress
+                adjusts[tabPosition] = progress
                 filterAdjuster = GPUImageFilterTools.FilterAdjuster(filters[tabPosition]!!)
                 filterAdjuster?.adjust(progress)
                 //gpuImage.requestRender()
-                imageFG.setImageBitmap(gpuImage.getBitmapWithFiltersApplied(imageManager.personOriginal,filters))
+                imageFG.setImageBitmap(
+                    gpuImage.getBitmapWithFiltersApplied(
+                        imageManager.personOriginal,
+                        filters
+                    )
+                )
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {}
@@ -80,16 +91,16 @@ class FragmentPerson : Fragment() {
         tabLayout.addOnTabSelectedListener(tabListener)
     }
 
-
-    public fun setImage(){
+    public fun setImage() {
         imageFG.setImageBitmap(imageManager.personOriginal)
         imageBG.setImageBitmap(imageManager.backgroundFiltered)
         gpuImage.setImage(imageManager.personOriginal)
 
     }
 
-    public fun saveImage(){
-        imageManager.personFiltered = gpuImage.getBitmapWithFiltersApplied(imageManager.personOriginal,filters)
+    public fun saveImage() {
+        imageManager.personFiltered =
+            gpuImage.getBitmapWithFiltersApplied(imageManager.personOriginal, filters)
     }
 
     override fun onCreateView(
@@ -101,15 +112,13 @@ class FragmentPerson : Fragment() {
     }
 
 
-
-    private fun addFilter(f: GPUImageFilter,index:Int) {
+    private fun addFilter(f: GPUImageFilter, index: Int) {
         var filter = f
-        if(filters[index]!=null){
+        if (filters[index] != null) {
             filter = filters[index]!!
             seekBar.progress = adjusts[index];
-        }else
-        {
-            filters[index]=f
+        } else {
+            filters[index] = f
         }
 
         filterAdjuster = GPUImageFilterTools.FilterAdjuster(filter)
@@ -119,45 +128,96 @@ class FragmentPerson : Fragment() {
             seekBar.visibility = View.GONE
         }
     }
-/*
-brightness 45~55
-contrast 40~60
-gamma 40~60
-sharpness 40~60
-saturation 0~100
-exposure 45~55
-highlight 0~50
-whitebalance 30~70
-vinette 50~70
-gausian blur -> 흐리
-Toon, SmoothToon -> 만화효과
-haze 40~60
-vibrance 25~75
- */
-    val tabListener = object : TabLayout.OnTabSelectedListener{
+
+    /*
+    brightness 45~55
+    contrast 40~60
+    gamma 40~60
+    sharpness 40~60
+    saturation 0~100
+    exposure 45~55
+    highlight 0~50
+    whitebalance 30~70
+    vinette 50~70
+    gausian blur -> 흐리
+    Toon, SmoothToon -> 만화효과
+    haze 40~60
+    vibrance 25~75
+     */
+    val tabListener = object : TabLayout.OnTabSelectedListener {
         override fun onTabReselected(tab: TabLayout.Tab?) {
         }
 
         override fun onTabUnselected(tab: TabLayout.Tab?) {}
 
         override fun onTabSelected(tab: TabLayout.Tab?) {
-            imageFG.setImageBitmap(gpuImage.getBitmapWithFiltersApplied(imageManager.personOriginal,filters))
-            seekBar.visibility=View.VISIBLE
-            tabPosition= tab!!.position
-            when(tab?.position){
-                0->{
+            imageFG.setImageBitmap(
+                gpuImage.getBitmapWithFiltersApplied(
+                    imageManager.personOriginal,
+                    filters
+                )
+            )
+            seekBar.visibility = View.VISIBLE
+            tabPosition = tab!!.position
+            when (tab?.position) {
+                0 -> {
                     imageFG.setImageBitmap(imageManager.personOriginal)
-                    seekBar.visibility=View.GONE
+                    seekBar.visibility = View.GONE
                 }
-                1-> addFilter(GPUImageFilterTools.createFilterForType(context!!,GPUImageFilterTools.FilterType.GAMMA),tab?.position)
-                2-> addFilter(GPUImageFilterTools.createFilterForType(context!!,GPUImageFilterTools.FilterType.SATURATION),tab?.position)
-                3-> addFilter(GPUImageFilterTools.createFilterForType(context!!,GPUImageFilterTools.FilterType.EXPOSURE),tab?.position)
-                4-> addFilter(GPUImageFilterTools.createFilterForType(context!!,GPUImageFilterTools.FilterType.HIGHLIGHT_SHADOW),tab?.position)
-                5-> addFilter(GPUImageFilterTools.createFilterForType(context!!,GPUImageFilterTools.FilterType.WHITE_BALANCE),tab?.position)
-                6-> addFilter(GPUImageFilterTools.createFilterForType(context!!,GPUImageFilterTools.FilterType.HAZE),tab?.position)
-                7-> addFilter(GPUImageFilterTools.createFilterForType(context!!,GPUImageFilterTools.FilterType.VIBRANCE),tab?.position)
+                1 -> addFilter(
+                    GPUImageFilterTools.createFilterForType(
+                        context!!,
+                        GPUImageFilterTools.FilterType.GAMMA
+                    ), tab?.position
+                )
+                2 -> addFilter(
+                    GPUImageFilterTools.createFilterForType(
+                        context!!,
+                        GPUImageFilterTools.FilterType.SATURATION
+                    ), tab?.position
+                )
+                3 -> addFilter(
+                    GPUImageFilterTools.createFilterForType(
+                        context!!,
+                        GPUImageFilterTools.FilterType.EXPOSURE
+                    ), tab?.position
+                )
+                4 -> addFilter(
+                    GPUImageFilterTools.createFilterForType(
+                        context!!,
+                        GPUImageFilterTools.FilterType.HIGHLIGHT_SHADOW
+                    ), tab?.position
+                )
+                5 -> addFilter(
+                    GPUImageFilterTools.createFilterForType(
+                        context!!,
+                        GPUImageFilterTools.FilterType.WHITE_BALANCE
+                    ), tab?.position
+                )
+                6 -> addFilter(
+                    GPUImageFilterTools.createFilterForType(
+                        context!!,
+                        GPUImageFilterTools.FilterType.HAZE
+                    ), tab?.position
+                )
+                7 -> addFilter(
+                    GPUImageFilterTools.createFilterForType(
+                        context!!,
+                        GPUImageFilterTools.FilterType.VIBRANCE
+                    ), tab?.position
+                )
+                8 -> {
+//                    imageManager.personOriginal =
+//                        imageHalo.setHalo(imageManager.personOriginal, imageManager.mask)
+                    val mutableImagePerson = imageManager.original.copy(Bitmap.Config.ARGB_8888,true)
+                    val mutableImageMask = imageManager.mask.copy(Bitmap.Config.ARGB_8888,true)
+                    imageManager.backgroundFiltered =
+                        imageHalo.setHalo(mutableImagePerson, mutableImageMask)
+                    seekBar.visibility = View.GONE
+                    setImage()
+                }
             }
-            seekBar.progress=adjusts[tabPosition]
+            seekBar.progress = adjusts[tabPosition]
             //gpuImage.requestRender()
         }
 
